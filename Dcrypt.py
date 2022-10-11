690da7d5b44e8cb3f96473f946df9a74f48800be
EmmanuelGitgud <arabitemmanuel5@gmail.com>
2022-10-11 23:13:03 +0800

    decrypts folder

--- Dcrypt.py
+++ Dcrypt.py
@@ -4,12 +4,14 @@ with open('key.json','rb') as f:
     key = f.read()
 key = Fernet(key)
 
-file = 'encrypted.jpg'
+file = 'topsecret.crypt'
+w_file = file.split('.')
+w_file = w_file[0]
+
 with open(file, 'rb') as f:
     data = f.read()
 
 decrypted = key.decrypt(data)
 
-
-with open (file, 'wb') as f:
+with open (w_file + '.zip', 'wb') as f:
     f.write(decrypted)
