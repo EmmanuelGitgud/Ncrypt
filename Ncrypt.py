d0ba738768ef25b74818873550a9535bb0f06f44
EmmanuelGitgud <arabitemmanuel5@gmail.com>
2022-10-21 17:27:12 +0800

    stable

--- Ncrypt.py
+++ Ncrypt.py
@@ -5,100 +5,71 @@ import os
 from tkinter import filedialog 
 from cryptography.fernet import Fernet
 
-def check_key():
+def check_key(path):
     # import key
     try:
-        with open('key.json','rb') as f:
-                key = f.read()
-        key = Fernet(key)
-        return key
+        with open(path + 'N.key','rb') as f:
+            f.read()
+            return True
     except:
         return False
 
-def encrypt_dir(directory):
+def encrypt_dir(directory, key):
     """function that encrypts directory"""
-    if check_key() is not False:
-        key = check_key()
-        # iterate through files inside directory
-        for filename in os.listdir(directory):
-            filestring = filename
+    with open(key + 'N.key','rb') as f:
+        key = f.read()
+    key = Fernet(key)
+        
+    for filename in os.listdir(directory):
+        str_fn = str(filename)
+        temp = filename.split('.')
+        file_name = temp[0]
+        file_type = temp[-1]
 
-            # splits file name into 2 parts
-            # (filename,file extension)
-            split_name = filename.split('.')
-            file_n = split_name[0]
-            file_x = split_name[-1]
+        if file_type != 'crypt':
+            #open file
+            with open(directory + filename, 'rb') as f:
+                filename = f.read()
 
-            # checks if file is already encrypted before encrypting it
-            if file_x != 'crypt':
-                #open file
-                with open(directory + filename, 'rb') as f:
-                    filename = f.read()
+            # encrypt each file each loop passes
+            token = key.encrypt(filename)
 
-                # encrypt each file each loop passes
-                token = key.encrypt(filename)
+            # write each encrypted file back
+            with open(directory + str_fn, 'wb') as f:
+                f.write(token)
 
-                # write each encrypted file back
-                with open(os.path.join(directory , file_n +'.'+ file_x +'.crypt'), 'wb') as f:
-                    f.write(token)
+            new_name = str(file_name +'.'+file_type + ".crypt")
+            os.rename(directory + str_fn, directory + new_name)
 
-                # remove the duplicate file after encrypting it
-                print(filestring + ' succesfully encrypted')
-                os.remove(directory + filestring)
-        print('\nWARNING: Do Not Lose Your Key')           
-        print('operation complete you may now close the window...')
+def decrypt_dir(directory, key):
+    with open(key + 'N.key','rb') as f:
+        key = f.read()
+    key = Fernet(key)
 
-def decrypt_dir(directory):
-    if check_key() is not False:
-        key = check_key()
-        # iterate through directory
-        for filename in os.listdir(directory):
-            filestring = filename
+    # iterate through directory
+    for filename in os.listdir(directory):
+        str_fn = str(filename)
+        temp = filename.split('.')
+        file_name = temp[0]
+        file_extension = temp[-2]
+        file_dummyX = temp[-1]
 
-            # splits file name into 3 parts
-            # (filename, original file extension, .crypt)
-            split_name = filename.split('.')
-            file_n = split_name[0]
-            file_x = split_name[-2]
-            file_c = split_name[-1]
+        if file_dummyX == 'crypt':
+            #open the file 
+            with open(directory + filename, 'rb') as f:
+                filename = f.read()
 
-            #checks if it is a crypt file before decrypting it
-            if file_c == 'crypt':
+            #decrypts the file
+            token = key.decrypt(filename)
 
-                #open the file 
-                with open(os.path.join(directory,filename), 'rb') as f:
-                    filename = f.read()
+            #write the file
+            with open(directory + str_fn,'wb') as f:
+                f.write(token)
 
-                #decrypts the file
-                token = key.decrypt(filename)
-
-                #write the file
-                with open(os.path.join(directory, file_n +'.'+ file_x),'wb') as f:
-                    f.write(token)
-
-                #remove the duplicate file after decrypting it
-                print(filestring + ' succesfully decrypted')
-                os.remove(directory + filestring)
-        print('\noperation complete you may now close the window...')
-
+            new_name = str(file_name+'.'+file_extension)
+            os.rename(directory + str_fn, directory + new_name)
+       
 def select_dir():
     """function for selecting file path"""
     filepath = filedialog.askdirectory()
     return filepath
-
-# print('********** Welcome to Ncrypt **********\n')
-
-# print('ENCRYPT - 1')
-# print('DECRYPT - 2\n')
-
-# user_in = input('please select 1 for encryption and 2 for decryption: ')
-# user_dir = input('please enter the file directory: ')
-
-# if user_in == '1':
-#     encrypt_dir(user_dir + '\\')
-
-# elif user_in == '2':
-#     decrypt_dir(user_dir + '\\')
-
-# else:
-#     print('invalid input!')
