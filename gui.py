2c5864a1e313eb527ff34206b75318b07a986de7
EmmanuelGitgud <arabitemmanuel5@gmail.com>
2022-10-21 14:51:24 +0800

    stable gui

--- gui.py
+++ gui.py
@@ -1,20 +1,49 @@
-from tkinter import filedialog
+import tkinter as tk
+from tkinter import filedialog, messagebox
 from tkinter import *
-from tkinter.tix import COLUMN
-from Ncrypt import select_dir
+from Ncrypt import *
 
 root = Tk()
 root.title('Ncrypt')
 root.resizable(False,False)
-# root.withdraw()
-# entry.insert(0,'enter directory')
 
+#functions
+def select_dir():
+    file_dir.delete(0,END)
+    filepath = filedialog.askdirectory()
+    file_dir.insert(0, filepath) 
+
+def select_key():
+    key_dir.delete(0,END)
+    filepath = filedialog.askdirectory()
+    key_dir.insert(0, filepath)
+
+def encrypt_cliked():
+    if check_key(key_dir.get()+'/') is not False:
+        path = os.path.realpath(file_loc.get())
+        os.startfile(path)
+        encrypt_dir(file_loc.get()+'/', key_loc.get()+'/')
+        messagebox.showinfo(title='encryption',message='encryption success you may close the window')
+    else:
+        messagebox.showerror('Error', 'Key not found in directory')
+def decrypt_clicked():
+    if check_key(key_dir.get()+'/') is not False:
+        path = os.path.realpath(file_loc.get())
+        os.startfile(path)
+        decrypt_dir(file_loc.get()+'/', key_loc.get()+'/')
+        messagebox.showinfo(title='decryption',message='decryption success you may close the window')
+    else:
+        messagebox.showerror('Error', 'Key not found in directory')
+        
 #font
 current_font = ("Arial Bold", 15)
 
 # declare
-file_dir = Entry(root,width=70)
-key_dir = Entry(root,width=70)
+key_loc = tk.Variable()
+file_loc = tk.Variable()
+
+file_dir = Entry(root,width=70,textvariable=file_loc)
+key_dir = Entry(root,width=70,textvariable=key_loc)
 
 browse_key_button = Button(root,font=current_font, text='Browse key',command=select_dir())
 browse_file_button = Button(root,font=current_font, text='Browse directory',command=select_dir())
